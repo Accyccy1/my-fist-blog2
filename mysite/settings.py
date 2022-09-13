4dddc34682918e0a0ae72a1b486bad21788cfd97
Accy <Accyccy1@gmail.com>
2022-09-13 22:03:19 +0800

    Uncommented the STATIC_ROOT variable in settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,11 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 #OMG ito lang pala dahilan kung bakit 404 css ko kasama yung import os
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, "static"),
+# )
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
